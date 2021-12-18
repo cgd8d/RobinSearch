f8ca0b95eb99933c16a06994149e1978931b8c27
cgd8d <Claytongdavis@gmail.com>
2021-12-17 20:51:12 -0500

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -1,6 +1,26 @@
 #include <typeinfo>
-#include <cstdint> 
-#include "mpfr.h"
+#include <cstdint>
+#include <cstdio>
+#include <exception>
+#include <mpfr.h>
 
 static_assert(typeid(mpfr_exp_t) == typeid(int64_t),
     "Type error: mpfr_exp_t");
+
+const mpfr_prec_t Precision = 128;
+
+/*
+Convention: All mpfr_t values should have their rounding
+mode appended, typically rndu or rndd.
+*/
+
+int main()
+{
+    int ret;
+    mpfr_t ExpGamma_rndu;
+    mpfr_init2(ExpGamma_rndu, Precision);
+    mpfr_const_euler(ExpGamma_rndu, MPFR_RNDU);
+    mpfr_exp(ExpGamma_rndu, ExpGamma_rndu, MPFR_RNDU);
+    ret = mpfr_printf("ExpGamma <= %.20RgU\n", ExpGamma_rndu);
+    if(ret < 0) throw std::runtime_error("failed output");
+}
