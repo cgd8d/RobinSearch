e27171e36d1d3c7297ff12a2c3e51b7f6c333a5f
cgd8d <Claytongdavis@gmail.com>
2021-12-27 17:30:00 -0500

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -443,7 +443,7 @@ Internally we record calculations of the critical
 epsilon, since the goal is to compute it for a very
 small subset of factors.
 */
-boost::circular_buffer<uint64_t> PrimeQueue(1024);
+boost::circular_buffer<uint64_t> PrimeQueue(2048);
 primesieve::iterator PrimeQueueProducer;
 struct PrimeQueueEpsilonGroup
 {
