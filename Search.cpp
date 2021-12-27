30c9cdfb4789865453116e9d896077d88297df42
cgd8d <Claytongdavis@gmail.com>
2021-12-27 17:34:25 -0500

    Try increasing buffer to 4096

--- Search.cpp
+++ Search.cpp
@@ -443,7 +443,7 @@ Internally we record calculations of the critical
 epsilon, since the goal is to compute it for a very
 small subset of factors.
 */
-boost::circular_buffer<uint64_t> PrimeQueue(2048);
+boost::circular_buffer<uint64_t> PrimeQueue(4096);
 primesieve::iterator PrimeQueueProducer;
 struct PrimeQueueEpsilonGroup
 {
