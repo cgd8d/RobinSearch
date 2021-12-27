811e7ddf384435696370e4360d72bc250c00a6c6
cgd8d <Claytongdavis@gmail.com>
2021-12-26 20:11:58 -0500

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -565,7 +565,7 @@ int main()
     PrimeQueueProducer.next_prime(); // discard value.
 
     // Continue processing.
-    while(Number_factors.front().Exp < 29)
+    while(Number_factors.front().Exp < 30)
     {
         while(true)
         {
