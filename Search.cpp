ffbe5af3fcfceda3a4071e0ba304af4e8b67b970
cgd8d <Claytongdavis@gmail.com>
2021-12-27 22:08:29 -0500

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -589,7 +589,7 @@ int main()
     cnt_NumUniquePrimeFactors++;
 
     // Continue processing.
-    while(Number_factors.front().Exp < 33) // 36)
+    while(Number_factors.front().Exp < 36)
     {
         while(true)
         {
