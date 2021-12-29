d429f45b98892bde5816baf538ecfeb7d9ad6a28
cgd8d <Claytongdavis@gmail.com>
2021-12-28 23:36:31 -0500

    Update FastBigFloat.hpp

--- FastBigFloat.hpp
+++ FastBigFloat.hpp
@@ -6,12 +6,18 @@ Support fast multiplication with multi-word precision.
 The trade-off is that we don't guarantee the MSB to be
 non-zero, only the most significant word.  Bad for addition,
 but ok for this use case.
+Also this struct represents strictly non-negative values.
+
+The struct represents:
+(sig[0] + sig[1]*2^64 + ...)*2^(64*exp)
+We guarantee sig[N-1] is non-zero unless the value is zero.
 */
 
 template <size_t N>
 struct FastBigFloat
 {
-    uint64_t exp;
+    int64_t exp;
+    std::array<uint64_t, N> sig;
 
 
 #endif
