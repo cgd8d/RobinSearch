71a1831103929574365f9d40cdcee88e4a22849a
cgd8d <Claytongdavis@gmail.com>
2022-01-01 23:42:06 -0500

    Update TestBigFloat.cpp

--- test/TestBigFloat.cpp
+++ test/TestBigFloat.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <exception>
 #include <mpfr.h>
-#include "../BigFloat.hpp"
+#include "../FastBigFloat.hpp"
 
-void CheckIntervals(BigFloat<3>& bf1, BigFloat<3>& bf2,
+void CheckIntervals(FastBigFloat<3>& bf1, FastBigFloat<3>& bf2,
                     mpfr_t mp1, mpfr_t mp2)
 {
     mpfr_t t1;
@@ -26,7 +26,7 @@ void CheckIntervals(BigFloat<3>& bf1, BigFloat<3>& bf2,
     mpfr_div(t1, t1, t2, MPFR_RNDD);
     if(mpfr_less_p(mp1, mp2) and mpfr_cmp_ui(t1, 10) > 0)
     {
-        throw std::runtime_error("BigFloat interval is too big.");
+        throw std::runtime_error("FastBigFloat interval is too big.");
     }
 }
 
@@ -38,7 +38,7 @@ int main()
     // Test starting at 1.
     for(size_t i = 0; i < 1000; i++)
     {
-        BigFloat<3> bf1, bf2;
+        FastBigFloat<3> bf1, bf2;
         mpfr_t mp1, mp2;
         bf1.set_ui(1);
         bf2.set_ui(1);
@@ -59,5 +59,5 @@ int main()
     }
 
 // Next, should test starting from arbitrary.
-    std::cout << "passed test of BigFloat." << std::endl;
+    std::cout << "passed test of FastBigFloat." << std::endl;
 }
