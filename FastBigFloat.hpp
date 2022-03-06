8a3f70823806263b15eb8d290092962ae1d6047f
cgd8d <Claytongdavis@gmail.com>
2022-03-05 20:27:40 -0500

    Update FastBigFloat.hpp

--- FastBigFloat.hpp
+++ FastBigFloat.hpp
@@ -60,14 +60,25 @@ struct FastBigFloat
         uint64_t carry = 0;
         uint64_t lo = _mulx_u64(std::get<0>(sig), x, reinterpret_cast<unsigned long long*>(&std::get<0>(sig)));
 
+        auto helperfunc = [](uint64_t& lo, uint64_t& hi, uint64_t& carry, const uint64_t& x)
+            {
+                uint64_t tmp = _mulx_u64(hi, x, reinterpret_cast<unsigned long long*>(&hi));
+                lo = __builtin_addcl(lo, tmp, carry, &carry);
+            };
+        std::apply([&](class T, auto... idx)
+            {
+                (helperfunc(std::get<idx>(sig), std::get<idx+1>(sig), carry, x),...);
+            },
+            std::make_index_sequence<N-1>{});
 
-
-
+/*
         for(size_t i = 1; i < N; i++)
         {
             uint64_t tmp = _mulx_u64(sig[i], x, reinterpret_cast<unsigned long long*>(&sig[i]));
             sig[i-1] = __builtin_addcl(sig[i-1], tmp, carry, &carry);
         }
+*/
+
         sig[N-1] += carry; // Will not overflow.
 
         /*
