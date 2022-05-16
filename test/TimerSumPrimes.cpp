d291a1ec031c44ab9840ef5580d44487f097d98c
cgd8d <Claytongdavis@gmail.com>
2022-05-16 19:29:35 -0400

    Create TimerSumPrimes.cpp

---
+++ test/TimerSumPrimes.cpp
@@ -0,0 +1,30 @@
+/// @example primesieve_iterator.cpp
+/// Iterate over primes using primesieve::iterator.
+/// This is based on libprimesieve with modifications.
+
+#include <primesieve.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  uint64_t limit = 135452688167ull;
+
+  primesieve::iterator it(0, limit);
+  uint64_t prime = it.next_prime();
+  uint64_t sum = 0;
+
+  // iterate over the primes below 10^9
+  for (; prime <= limit; prime = it.next_prime())
+    sum += prime;
+
+  std::cout << "Sum of primes <= " << limit << " = " << sum << std::endl;
+
+  // Note that since sum is a 64-bit variable the result
+  // will be incorrect (due to integer overflow) if
+  // limit > 10^10. However we do allow limits > 10^10
+  // since this is useful for benchmarking.
+  if (limit > 10000000000ull)
+    std::cerr << "Warning: sum is likely incorrect due to 64-bit integer overflow!" << std::endl;
+
+  return 0;
+}
