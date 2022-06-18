8a4086855d6f88482c8f43002b79c1e7627b2b58
cgd8d <Claytongdavis@gmail.com>
2022-06-18 13:57:25 -0400

    Update SetupGMP.sh

--- scripts/SetupGMP.sh
+++ scripts/SetupGMP.sh
@@ -5,8 +5,8 @@ tar -xf gmp-6.2.1.tar.xz
 cd gmp-6.2.1
 ./configure --help
 CC=clang-12 CFLAGS="-fuse-ld=gold -flto=thin -march=native -O3" AR=llvm-ar-12 NM=llvm-nm-12 RANLIB=llvm-ranlib-12 ./configure --disable-shared --disable-fft
-make
-make check
+make -j --output-sync
+make -j --output-sync check
 #cd tune
 #make speed
 ls -a
