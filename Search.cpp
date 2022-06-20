707573c42d3a99b63ef3899217e6353acc9fe040
cgd8d <Claytongdavis@gmail.com>
2022-06-20 10:45:33 -0400

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -494,7 +494,7 @@ small subset of factors.
 std::vector<std::vector<uint64_t>> PrimeQueueVec(NumThreads);
 size_t PrimeQueueStep = 1 << 15;
 const size_t TargetPrimeQueueSize = 1 << 14;
-uint64_t NextPrimeToGen = 0;
+uint64_t NextPrimeToGen = 3;
 size_t PrimeQueueVecIdx = NumThreads-1;
 size_t MaxPrimeQueueDiff = 0; // Track range of primes.
 size_t NextPrimeIdx = 0;
