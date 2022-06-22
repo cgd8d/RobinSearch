1ec5f59d5464e8f52737c9119d81d4da62df8bd1
cgd8d <Claytongdavis@gmail.com>
2022-06-22 19:12:48 -0400

    Update TimerMulPrimes2.cpp

--- test/TimerMulPrimes2.cpp
+++ test/TimerMulPrimes2.cpp
@@ -4,11 +4,8 @@
 #include <primesieve.hpp>
 #include <mpfr.h>
 #include <iostream>
-#include <vector>
 
 const mpfr_prec_t Precision = 127;
-
-std::vector<uint64_t> PrimeQueue(1 << 14);
 
 std::ostream& operator<<(std::ostream& os, mpfr_t op)
 {
@@ -30,81 +27,20 @@ int main(int argc, char** argv)
 {
   uint64_t limit = 135452688167ull;
 
-  primesieve::iterator it_exp(0, 1000);
-
   primesieve::iterator it;
   uint64_t prime = it.next_prime();
   mpfr_t prod;
   mpfr_init2(prod, Precision);
   mpfr_set_ui(prod, 1, MPFR_RNDU);
-  while(prime < (1ull << 36))
-
-
-
-
-
-
-
-
-
   mpfr_t tmp;
   mpfr_init2(tmp, Precision);
-
-  while(true)
+  while(prime < limit)
   {
-        size_t i = 0;
-        while(i < PrimeQueue.size())
-        {
-            // Hack into primesieve iterator to enable
-            // fast copy.
-            // Handle i_ the way iterator usually does
-            // so the iterator remains in a valid state.
-            if(it.i_++ == it.last_idx_)
-            {
-                // Note generate_next_primes has post-
-                // condition that i_ == 0.
-                it.generate_next_primes();
-            }
-            size_t num_copy = std::min(
-                PrimeQueue.size()-i,
-                it.last_idx_+1-it.i_);
-            std::copy(
-                it.primes_.begin()+it.i_,
-                it.primes_.begin()+it.i_+num_copy,
-                PrimeQueue.begin()+i);
-            it.i_ += num_copy-1;
-            i += num_copy;
-        }
-        NextPrimeIdx = 0;
+    mpfr_set_ui(tmp, prime, MPFR_RNDU); // exact
+    mpfr_mul(prod, prod, tmp, MPFR_RNDD);
+    prime = it.next_prime();
+  }
 
-
-        i = 0;
-        while(i < PrimeQueue.size())
-        {
-          if(PrimeQueue[i] > limit)
-          {
-            std::cout << "Sum of primes <= " << limit << " = " << sum << std::endl;
-
-            // Note that since sum is a 64-bit variable the result
-            // will be incorrect (due to integer overflow) if
-            // limit > 10^10. However we do allow limits > 10^10
-            // since this is useful for benchmarking.
-            if (limit > 10000000000ull)
-              std::cerr << "Warning: sum is likely incorrect due to 64-bit integer overflow!" << std::endl;
-
-            return 0;
-          }
-
-          mpfr_set_ui(tmp, PrimeQueue[i], MPFR_RNDU); // exact
-          mpfr_mul(sum, sum, tmp, MPFR_RNDD);
-          i++;
-       }
-
-      // Does another prime iterator mess up the cache?
-      mpfr_set_ui(tmp, it_exp.next_prime(), MPFR_RNDU); // exact
-      mpfr_mul(sum, sum, tmp, MPFR_RNDD);
-    }
-
-
+  std::cout << "Product of primes <= " << limit << " = " << prod << std::endl;
   return 0;
 }
