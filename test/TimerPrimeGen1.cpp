08a4703ae5b1f471a9564a8cb3e12e01753721ee
cgd8d <Claytongdavis@gmail.com>
2022-06-24 20:28:08 -0400

    Update TimerPrimeGen1.cpp

--- test/TimerPrimeGen1.cpp
+++ test/TimerPrimeGen1.cpp
@@ -7,6 +7,29 @@
 #include <vector>
 #include <numeric>
 
+template <typename T>
+void store_primes_modified(uint64_t start,
+                         uint64_t stop,
+                         T& primes)
+{
+  if (start > 0)
+    start--;
+  if (~stop == 0)
+    stop--;
+
+  if (start < stop)
+  {
+    using V = typename T::value_type;
+    std::size_t size = primes.size() + primesieve::prime_count_approx(start, stop);
+    primes.reserve(size);
+
+    primesieve::iterator it(start, stop);
+    uint64_t prime = it.next_prime();
+    for (; prime <= stop; prime = it.next_prime())
+      primes.push_back((V) prime);
+  }
+}
+
 int main(int argc, char** argv)
 {
   uint64_t num_iter = (1 << 7);
@@ -17,10 +40,10 @@ int main(int argc, char** argv)
   for(uint64_t i = 0; i < num_iter; i++)
   {
     vec.clear();
-    primesieve::generate_primes(
+    store_primes_modified(
       i*step_size,
       (i+1)*step_size,
-      &vec);
+      vec);
     sum = std::accumulate(vec.begin(), vec.end(), sum);
   }
 
