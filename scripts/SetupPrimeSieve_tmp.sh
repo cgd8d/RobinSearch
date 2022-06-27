af22ffc0df18c4454e96dc758510ccb196e78b13
cgd8d <Claytongdavis@gmail.com>
2022-06-27 19:22:46 -0400

    Update SetupPrimeSieve_tmp.sh

--- scripts/SetupPrimeSieve_tmp.sh
+++ scripts/SetupPrimeSieve_tmp.sh
@@ -1,7 +1,8 @@
 # Compile libprimesieve from source.
 
-git clone --depth 1 --branch 1bc574635fc755c8e3f0a0e887de0d4459743eb1 https://github.com/kimwalisch/primesieve
+git clone --depth 1 https://github.com/kimwalisch/primesieve
 cd primesieve
+git checkout 1bc574635fc755c8e3f0a0e887de0d4459743eb1
 CC="clang-12 -march=native" CXX="clang++-12 -march=native" cmake -DBUILD_TESTS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j
 ctest
