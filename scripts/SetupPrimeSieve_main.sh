043ddc2f2b2275bf37b2e0822ff0d5259f2f24db
cgd8d <Claytongdavis@gmail.com>
2022-06-28 21:00:42 -0400

    Update SetupPrimeSieve_main.sh

--- scripts/SetupPrimeSieve_main.sh
+++ scripts/SetupPrimeSieve_main.sh
@@ -2,7 +2,7 @@
 
 git clone --depth 1 https://github.com/kimwalisch/primesieve
 cd primesieve
-CC="clang-12 -march=native" CXX="clang++-12 -march=native" cmake -DBUILD_TESTS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
+CC="clang-12 -march=native -Wall -Wextra" CXX="clang++-12 -march=native -Wall -Wextra" cmake -DBUILD_TESTS=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j
 ctest
 echo "Run ls"
