5fc798e4136ea27bae124caba930cdcd8eb0d31e
cgd8d <Claytongdavis@gmail.com>
2022-09-06 18:38:39 -0400

    Update SetupPrimeSieve.sh

--- scripts/SetupPrimeSieve.sh
+++ scripts/SetupPrimeSieve.sh
@@ -1,9 +1,9 @@
 # Compile libprimesieve from source.
 
-git clone --depth 1 --branch v7.9 https://github.com/kimwalisch/primesieve
+git clone --depth 1 --branch v8.0 https://github.com/kimwalisch/primesieve
 cd primesieve
 sed -i 's/private/public/g' include/primesieve/iterator.hpp
-CC="clang-12 -fuse-ld=gold -march=native" CXX="clang++-12 -fuse-ld=gold -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
+CC="clang-14 -fuse-ld=gold -march=native" CXX="clang++-14 -fuse-ld=gold -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j
 ctest
 echo "Run ls"
