aeb7d71f65feeca48f203757d1d4258bce70fa89
cgd8d <Claytongdavis@gmail.com>
2023-02-18 00:00:20 -0500

    Update SetupMPFR.sh

--- scripts/SetupMPFR.sh
+++ scripts/SetupMPFR.sh
@@ -2,11 +2,11 @@
 # Parallel build doesn't seem to speed it up (6/18/2022),
 # but left here anyway.
 
-git clone --depth 1 --branch 4.1 https://gitlab.inria.fr/mpfr/mpfr
+git clone --depth 1 --branch 4.2 https://gitlab.inria.fr/mpfr/mpfr
 cd mpfr
 autoreconf -i
 ./configure --help
-CC=clang-14 CFLAGS="-fuse-ld=gold -flto=thin -march=native -O3" AR=llvm-ar-14 NM=llvm-nm-14 RANLIB=llvm-ranlib-14 ./configure --with-gmp-build=${PWD%/*}/gmp-6.2.1 --disable-shared --disable-decimal-float --disable-float128
+CC=clang-15 CFLAGS="-fuse-ld=gold -flto=thin -march=native -O3" AR=llvm-ar-15 NM=llvm-nm-15 RANLIB=llvm-ranlib-15 ./configure --with-gmp-build=${PWD%/*}/gmp-6.2.1 --disable-shared --disable-decimal-float --disable-float128
 #echo "make tune"
 #cd tune
 #make tune
