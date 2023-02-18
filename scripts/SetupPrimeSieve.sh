dc52a4c8283b2c213cd86b48879fd937d7ad5c77
cgd8d <Claytongdavis@gmail.com>
2023-02-18 12:47:14 -0500

    Update SetupPrimeSieve.sh

--- scripts/SetupPrimeSieve.sh
+++ scripts/SetupPrimeSieve.sh
@@ -4,7 +4,8 @@ git clone --depth 1 --branch v11.0 https://github.com/kimwalisch/primesieve
 cp -f next_prime2.c primesieve/test/next_prime2.c
 cd primesieve
 sed -i 's/private/public/g' include/primesieve/iterator.hpp
-CC="clang-15 -fuse-ld=gold -march=native" CXX="clang++-15 -fuse-ld=gold -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
+CC="clang-15 -march=native" CXX="clang++-15 -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
+# CC="clang-15 -fuse-ld=gold -march=native" CXX="clang++-15 -fuse-ld=gold -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j
 #ctest
 echo "starting next_prime2"
