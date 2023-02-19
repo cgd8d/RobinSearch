046ff1ab36e8581097c0a8b2a05fdd799007a02d
cgd8d <Claytongdavis@gmail.com>
2023-02-19 08:56:05 -0500

    Update SetupPrimeSieve.sh

--- scripts/SetupPrimeSieve.sh
+++ scripts/SetupPrimeSieve.sh
@@ -5,29 +5,11 @@
 # gcc LTO does seem to work, if I needed it later.
 
 git clone --depth 1 --branch v11.0 https://github.com/kimwalisch/primesieve
-#cp -f next_prime2.c primesieve/test/next_prime2.c
 cd primesieve
-
-#CC=clang-15 CXX=clang++-15 cmake -DBUILD_TESTS=ON -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE -DCMAKE_BUILD_TYPE=Debug .
-#make -j VERBOSE=1
-#ctest
-
-#exit 1
-
 sed -i 's/private/public/g' include/primesieve/iterator.hpp
 CC="clang-15 -march=native" CXX="clang++-15 -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
-# CC="clang-15 -fuse-ld=gold -march=native" CXX="clang++-15 -fuse-ld=gold -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j VERBOSE=1
 ctest
-
-#echo "Starting generate_primes2"
-#./test/generate_primes2
-
-#echo "Starting skipto_next_prime"
-#./test/skipto_next_prime
-
-#echo "starting next_prime2"
-#./test/next_prime2
 
 echo "Run ls"
 ls
