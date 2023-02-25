669cb3690fa23b52aa56579cff6dc8fa2442aa9d
cgd8d <Claytongdavis@gmail.com>
2023-02-25 01:05:10 -0500

    Update mpfr_mul_ui_fast.hpp

--- mpfr_mul_ui_fast.hpp
+++ mpfr_mul_ui_fast.hpp
@@ -37,7 +37,18 @@ int mpfr_mul_ui_fast (mpfr_ptr x, unsigned long int u, mpfr_rnd_t rnd_mode)
     magic<N+1, unsigned long int>::tuple_type{} tmp;
 
     // X data is stored as little endian
+
+    unsigned long int out0, out1, out2;
+    unsigned long int p0, p1, p2;
+    unsigned char c0;
+    mp_limb_t *xp = MPFR_MANT(x);
+    out0 = _mulx_u64(xp[0], u, &p0);
+    p1 = _mulx_u64(xp[0], u, &p2);
+    c0 = _addcarryx_u64(0, p0, p1, &out1);
+    out2 = p2 + c0;
+
 ... To finish later 
+
 
 
 }
