7c3edf4868b052c84fcebe673cb924ef2b3aab85
cgd8d <Claytongdavis@gmail.com>
2023-02-24 19:02:52 -0500

    Create CompileRobinSearch.sh

---
+++ scripts/CompileRobinSearch.sh
@@ -0,0 +1,16 @@
+# Compile RobinSearch including profile-guided
+# optimization.
+
+clang++-15 --version
+/usr/bin/ld -v
+lscpu
+# Get macros only.
+clang++-15 -march=native -E - -dM </dev/null # print macros
+# Compile and run instrumented version for PGO.
+clang++-15 -fprofile-instr-generate -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lprimesieve -o Search
+echo "Starting instrumented run."
+time ./Search 34
+llvm-profdata-15 merge -output=code.profdata default.profraw
+# Now compile the real version.
+clang++-15 -fprofile-instr-use=code.profdata -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lprimesieve -o Search
+#clang++ -std=c++20 -DNDEBUG -Wall -O3 -flto -march=native -lmpfr -lprimesieve Search.cpp -o Search_ndebug
