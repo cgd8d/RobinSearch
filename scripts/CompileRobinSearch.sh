57596afec5ba57823da85622f774c6817244274f
cgd8d <Claytongdavis@gmail.com>
2023-03-19 18:46:14 -0400

    Update CompileRobinSearch.sh

--- scripts/CompileRobinSearch.sh
+++ scripts/CompileRobinSearch.sh
@@ -9,7 +9,7 @@ clang++-15 -march=native -E - -dM </dev/null # print macros
 # Compile and run instrumented version for PGO.
 clang++-15 -fprofile-instr-generate -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lprimesieve -o Search
 echo "Starting instrumented run."
-time ./Search 35
+time ./Search 37
 llvm-profdata-15 merge -output=code.profdata default.profraw
 # Now compile the real version.
 clang++-15 -fprofile-instr-use=code.profdata -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lprimesieve -o Search
