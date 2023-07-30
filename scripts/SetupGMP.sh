91c118dc40bedc778e257ff464cdf0faeff44f6f
cgd8d <Claytongdavis@gmail.com>
2023-07-30 18:40:58 -0400

    Update SetupGMP.sh

--- scripts/SetupGMP.sh
+++ scripts/SetupGMP.sh
@@ -1,8 +1,8 @@
 # Install GMP from source.
 
-wget https://ftp.gnu.org/gnu/gmp/gmp-6.2.1.tar.xz
-tar -xf gmp-6.2.1.tar.xz
-cd gmp-6.2.1
+wget https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
+tar -xf gmp-6.3.0.tar.xz
+cd gmp-6.3.0
 ./configure --help
 CC=clang-15 CFLAGS="-fuse-ld=gold -flto=thin -march=native -O3" AR=llvm-ar-15 NM=llvm-nm-15 RANLIB=llvm-ranlib-15 ./configure --disable-shared --disable-fft
 make -j --output-sync
