22eb2106bff7a2f643a23c5ccfd17572f25650ed
cgd8d <Claytongdavis@gmail.com>
2023-07-30 18:41:44 -0400

    Update SetupMPFR.sh

--- scripts/SetupMPFR.sh
+++ scripts/SetupMPFR.sh
@@ -6,7 +6,7 @@ git clone --depth 1 --branch 4.2 https://gitlab.inria.fr/mpfr/mpfr
 cd mpfr
 autoreconf -i
 ./configure --help
-CC=clang-15 CFLAGS="-fuse-ld=gold -flto=thin -march=native -O3" AR=llvm-ar-15 NM=llvm-nm-15 RANLIB=llvm-ranlib-15 ./configure --with-gmp-build=${PWD%/*}/gmp-6.2.1 --disable-shared --disable-decimal-float --disable-float128
+CC=clang-15 CFLAGS="-fuse-ld=gold -flto=thin -march=native -O3" AR=llvm-ar-15 NM=llvm-nm-15 RANLIB=llvm-ranlib-15 ./configure --with-gmp-build=${PWD%/*}/gmp-6.3.0 --disable-shared --disable-decimal-float --disable-float128
 #echo "make tune"
 #cd tune
 #make tune
