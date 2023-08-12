9913d671c1384f50b20d89025d862ca8807824ba
cgd8d <Claytongdavis@gmail.com>
2023-08-12 08:44:44 -0400

    Update SetupPrimeSieve.sh

--- scripts/SetupPrimeSieve.sh
+++ scripts/SetupPrimeSieve.sh
@@ -7,7 +7,8 @@
 git clone --depth 1 --branch v11.1 https://github.com/kimwalisch/primesieve
 cd primesieve
 sed -i 's/private/public/g' include/primesieve/iterator.hpp
-sed -i 's/BUCKET_BYTES = 8 << 10/BUCKET_BYTES = 8 << 11/' include/primesieve/config.hpp
+#sed -i 's/BUCKET_BYTES = 8 << 10/BUCKET_BYTES = 8 << 11/' include/primesieve/config.hpp
+sed -i 's/FACTOR_ERATSMALL = 0.2/FACTOR_ERATSMALL = 0.15/' include/primesieve/config.hpp
 cat include/primesieve/config.hpp
 CC="clang-15 -march=native" CXX="clang++-15 -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j VERBOSE=1
