d5e644dc4cf7fc1330d7e39f222ca2a74c90b1ae
cgd8d <Claytongdavis@gmail.com>
2024-01-04 21:20:00 -0500

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -1135,7 +1135,7 @@ int main(int argc, char *argv[])
               << primesieve::cpuInfo.l3CacheBytes()
               << " bytes (L3)"
               << std::endl;
-    primesieve::set_sieve_size(1<<10);
+    //primesieve::set_sieve_size(1<<10);
     std::cout << "Prime sieve size changed to "
               << primesieve::get_sieve_size()
               << " KiB (kibibytes)."
