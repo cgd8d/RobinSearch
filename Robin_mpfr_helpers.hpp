24cda1e73918ffb6ef77164d78da2c01977e8b66
cgd8d <Claytongdavis@gmail.com>
2024-01-21 15:13:07 -0500

    Create Robin_mpfr_helpers.hpp

---
+++ Robin_mpfr_helpers.hpp
@@ -0,0 +1,63 @@
+#ifndef Robin_mpfr_helpers_hpp
+#define Robin_mpfr_helpers_hpp
+
+#include <mpfr.h>
+
+const mpfr_prec_t Precision = 128;
+
+// Helper object to store initialized mpfr objects.
+// Provide correct copy semantics.
+// Also provide implicit conversion to mpfr_t.
+struct mpfr_holder
+{
+    MPFR_DECL_INIT(val, Precision);
+
+    mpfr_holder() = default;
+    mpfr_holder(const mpfr_holder& src)
+    {
+        *val = *src.val;
+        val->_mpfr_d = __gmpfr_local_tab_val;
+        for(size_t i = 0;
+            i < NumLimbs;
+            i++)
+        {
+            __gmpfr_local_tab_val[i] = src.__gmpfr_local_tab_val[i];
+        }
+    }
+    mpfr_holder& operator=(mpfr_holder& src)
+    {
+        *val = *src.val;
+        val->_mpfr_d = __gmpfr_local_tab_val;
+        for(size_t i = 0;
+            i < NumLimbs;
+            i++)
+        {
+            __gmpfr_local_tab_val[i] = src.__gmpfr_local_tab_val[i];
+        }
+        return *this;
+    }
+
+    operator mpfr_t&()
+    {
+        return val;
+    }
+
+    operator const mpfr_t&() const
+    {
+        return val;
+    }
+
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        // I'm not completely sure what the default
+        // constructor catches, so being conservative here.
+        ar & val->_mpfr_prec; // might be redundant
+        ar & val->_mpfr_exp;
+        ar & val->_mpfr_sign;
+        ar & __gmpfr_local_tab_val;
+        val->_mpfr_d = __gmpfr_local_tab_val; // might be redundant...
+    }
+};
+
+#endif
