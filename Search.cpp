a5ca55dd64f6186027b291fb217769a1e3710762
cgd8d <Claytongdavis@gmail.com>
2024-01-21 19:16:37 -0500

    Update Search.cpp

--- Search.cpp
+++ Search.cpp
@@ -19,9 +19,9 @@
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/list.hpp>
 #include "PlotDelta.hpp"
+#include "Robin_mpfr_helpers.hpp"
 #include "mpfr_mul_ui_fast.hpp"
 
-const mpfr_prec_t Precision = 128;
 const size_t NumLimbs = 2;
 const int NumThreads = 4;
 
@@ -71,60 +71,6 @@ void CheckTypes()
         "Precision and NumLimbs are not consistent.");
 }
 
-// Helper object to store initialized mpfr objects.
-// Provide correct copy semantics.
-// Also provide implicit conversion to mpfr_t.
-struct mpfr_holder
-{
-    MPFR_DECL_INIT(val, Precision);
-
-    mpfr_holder() = default;
-    mpfr_holder(const mpfr_holder& src)
-    {
-        *val = *src.val;
-        val->_mpfr_d = __gmpfr_local_tab_val;
-        for(size_t i = 0;
-            i < NumLimbs;
-            i++)
-        {
-            __gmpfr_local_tab_val[i] = src.__gmpfr_local_tab_val[i];
-        }
-    }
-    mpfr_holder& operator=(mpfr_holder& src)
-    {
-        *val = *src.val;
-        val->_mpfr_d = __gmpfr_local_tab_val;
-        for(size_t i = 0;
-            i < NumLimbs;
-            i++)
-        {
-            __gmpfr_local_tab_val[i] = src.__gmpfr_local_tab_val[i];
-        }
-        return *this;
-    }
-
-    operator mpfr_t&()
-    {
-        return val;
-    }
-
-    operator const mpfr_t&() const
-    {
-        return val;
-    }
-
-    template<class Archive>
-    void serialize(Archive & ar, const unsigned int version)
-    {
-        // I'm not completely sure what the default
-        // constructor catches, so being conservative here.
-        ar & val->_mpfr_prec; // might be redundant
-        ar & val->_mpfr_exp;
-        ar & val->_mpfr_sign;
-        ar & __gmpfr_local_tab_val;
-        val->_mpfr_d = __gmpfr_local_tab_val; // might be redundant...
-    }
-};
 std::vector<mpfr_holder> mpfr_tmp;
 
 // Helper function to print mpfr_t with error checking.
