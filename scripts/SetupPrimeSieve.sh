9f8c8f69bfa6abef1a4e2b4395cb9266b545a52b
cgd8d <Claytongdavis@gmail.com>
2024-05-27 23:26:05 -0400

    Update SetupPrimeSieve.sh

--- scripts/SetupPrimeSieve.sh
+++ scripts/SetupPrimeSieve.sh
@@ -7,7 +7,7 @@
 git clone --depth 1 --branch v11.1 https://github.com/kimwalisch/primesieve
 cd primesieve
 sed -i 's/private/public/g' include/primesieve/iterator.hpp
-CC="clang-15 -march=native" CXX="clang++-15 -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
+CC="clang-18 -march=native" CXX="clang++-18 -march=native" cmake -DBUILD_TESTS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON .
 make -j VERBOSE=1
 ctest
 
