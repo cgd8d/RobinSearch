03d455e3237ffcaf54dbb62d2f3f176301532e7b
cgd8d <Claytongdavis@gmail.com>
2024-05-27 23:22:17 -0400

    Update CompileRobinSearch.sh

--- scripts/CompileRobinSearch.sh
+++ scripts/CompileRobinSearch.sh
@@ -1,17 +1,17 @@
 # Compile RobinSearch including profile-guided
 # optimization.
 
-clang++-15 --version
+clang++-18 --version
 /usr/bin/ld -v
 lscpu
 # Get macros only.
-clang++-15 -march=native -E - -dM </dev/null # print macros
+clang++-18 -march=native -E - -dM </dev/null # print macros
 # Compile and run instrumented version for PGO.
-clang++-15 -fprofile-instr-generate -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lboost_serialization -lprimesieve -o Search
+clang++-18 -fprofile-instr-generate -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lboost_serialization -lprimesieve -o Search
 echo "Starting instrumented run."
 PROF_JOB_STOP_TIME=`date --date="+2 mins" +%s`
 time ./Search 60 $PROF_JOB_STOP_TIME checkpoints/Checkpoint_42hrs_20231211.txt
-llvm-profdata-15 merge -output=code.profdata default.profraw
+llvm-profdata-18 merge -output=code.profdata default.profraw
 # Now compile the real version.
-clang++-15 -fprofile-instr-use=code.profdata -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lboost_serialization -lprimesieve -o Search
+clang++-18 -fprofile-instr-use=code.profdata -std=c++20 -fuse-ld=gold -g -Wall -O3 -flto=thin -march=native -Wl,-plugin-opt=whole-program-visibility -fopenmp $MY_DEPENDENCY_FLAGS Search.cpp -lmpfr -lgmp -lboost_serialization -lprimesieve -o Search
 #clang++ -std=c++20 -DNDEBUG -Wall -O3 -flto -march=native -lmpfr -lprimesieve Search.cpp -o Search_ndebug
