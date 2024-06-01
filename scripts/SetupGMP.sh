a77d1bfbe49e7b3b12ebeb854c8c821e6f065a52
cgd8d <Claytongdavis@gmail.com>
2024-05-31 21:09:13 -0400

    Update SetupGMP.sh

--- scripts/SetupGMP.sh
+++ scripts/SetupGMP.sh
@@ -4,7 +4,7 @@ wget https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
 tar -xf gmp-6.3.0.tar.xz
 cd gmp-6.3.0
 ./configure --help
-CC=clang-18 CFLAGS="-march=native -O2" AR=llvm-ar-18 NM=llvm-nm-18 RANLIB=llvm-ranlib-18 ./configure --disable-shared --disable-fft
+CC=clang-$MY_LLVM_VER CFLAGS="-march=native -O2" AR=llvm-ar-$MY_LLVM_VER NM=llvm-nm-$MY_LLVM_VER RANLIB=llvm-ranlib-$MY_LLVM_VER ./configure --disable-shared --disable-fft
 make -j --output-sync
 make -j --output-sync check
 #cd tune
