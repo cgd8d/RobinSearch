b97257043b8bda12383d3302c8221c8c21ddf607
cgd8d <Claytongdavis@gmail.com>
2024-06-19 19:30:29 -0400

    Update PrimeCacheClobber.cpp

--- test/PrimeCacheClobber.cpp
+++ test/PrimeCacheClobber.cpp
@@ -9,7 +9,7 @@
 
 uint64_t func1(uint64_t start, uint64_t stop)
 {
-  uint64_t acc = 0
+  uint64_t acc = 0;
   primesieve::iterator it(start);
   uint64_t prime = it.next_prime();
   for (; prime < stop; prime = it.next_prime())
@@ -35,13 +35,13 @@ void TimeFunc(F func, std::string fname)
   uint64_t nchunks = (1ull << 10);
   uint64_t sum_mod64 = 0;
   
-  auto t1 = std::high_resolution_clock::now();
+  auto t1 = std::chrono::high_resolution_clock::now();
   for(size_t i = 0; i < nchunks; i++) {
     uint64_t start = i*step_per_chunk;
     uint64_t stop = start+step_per_chunk;
     sum_mod64 += func(start, stop);
   }
-  auto t2 = std::high_resolution_clock::now();
+  auto t2 = std::chrono::high_resolution_clock::now();
 
   std::cout << "Using " << fname
     << ", sum of primes < " << step_per_chunk*nchunks
