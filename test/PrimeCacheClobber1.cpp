1d07dff308956fca6900d361c41b5b8320832d12
cgd8d <Claytongdavis@gmail.com>
2024-06-18 23:39:13 -0400

    Update PrimeCacheClobber1.cpp

--- test/PrimeCacheClobber1.cpp
+++ test/PrimeCacheClobber1.cpp
@@ -4,24 +4,43 @@
 
 #include <primesieve.hpp>
 #include <iostream>
+#include <chrono>
 
-int main(int argc, char** argv)
+uint64_t func1(uint64_t start, uint64_t stop)
+{
+  uint64_t acc = 0
+  primesieve::iterator it(start);
+  uint64_t prime = it.next_prime();
+  for (; prime < stop; prime = it.next_prime())
+    acc += prime;
+  return acc;
+}
+
+template<typename F>
+void TimeFunc(F func, std::string fname)
 {
   uint64_t step_per_chunk = (1ull << 27);
   uint64_t nchunks = (1ull << 10);
   uint64_t sum_mod64 = 0;
-
+  
+  auto t1 = std::high_resolution_clock::now();
   for(size_t i = 0; i < nchunks; i++) {
     uint64_t start = i*step_per_chunk;
-    uint64_t end = start+step_per_chunk;
-    primesieve::iterator it(start);
-
-    uint64_t prime = it.next_prime();
-    for (; prime < limit; prime = it.next_prime())
-      sum_mod64 += prime;
+    uint64_t stop = start+step_per_chunk;
+    sum_mod64 += func(start, stop);
   }
-  
-  std::cout << "Sum of primes < " << step_per_chunk*nchunks << " (mod 2^64) = " << sum_mod64 << std::endl;
+  auto t2 = std::high_resolution_clock::now();
+
+  std::cout << "Using " << fname
+    << ", sum of primes < " << step_per_chunk*nchunks
+    << " (mod 2^64) = " << sum_mod64
+    << " computed in " << t2-t1
+    << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+  TimeFunc(func1, "func1");
 
   return 0;
 }
