21b9e8e66b78a12342a1186011fe3b93f1c96d94
cgd8d <Claytongdavis@gmail.com>
2024-06-18 22:40:04 -0400

    Create PrimeCacheClobber1.cpp
    Begin writing tests to show that writing primes to vector clobbers cache.

---
+++ test/PrimeCacheClobber1.cpp
@@ -0,0 +1,27 @@
+/// @example primesieve_iterator.cpp
+/// Iterate over primes using primesieve::iterator.
+/// This is based on libprimesieve with modifications.
+
+#include <primesieve.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  uint64_t step_per_chunk = (1ull << 27);
+  uint64_t nchunks = (1ull << 10);
+  uint64_t sum_mod64 = 0;
+
+  for(size_t i = 0; i < nchunks; i++) {
+    uint64_t start = i*step_per_chunk;
+    uint64_t end = start+step_per_chunk;
+    primesieve::iterator it(start);
+
+    prime = it.next_prime();
+    for (; prime < limit; prime = it.next_prime())
+      sum_mod64 += prime;
+  }
+  
+  std::cout << "Sum of primes < " << step_per_chunk*nchunks << " (mod 2^64) = " << sum_mod64 << std::endl;
+
+  return 0;
+}
