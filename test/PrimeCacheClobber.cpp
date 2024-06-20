242a810205a2659c8532d33d3a45318cecea1daf
cgd8d <Claytongdavis@gmail.com>
2024-06-19 20:54:38 -0400

    Update PrimeCacheClobber.cpp

--- test/PrimeCacheClobber.cpp
+++ test/PrimeCacheClobber.cpp
@@ -33,6 +33,7 @@ template<typename F>
 void TimeFunc(
     F func,
     std::string fname,
+    uint64_t start,
     uint64_t step_per_chunk,
     uint64_t nchunks
 )
@@ -41,15 +42,16 @@ void TimeFunc(
   
   auto t1 = std::chrono::high_resolution_clock::now();
   for(size_t i = 0; i < nchunks; i++) {
-    uint64_t start = i*step_per_chunk;
-    uint64_t stop = start+step_per_chunk;
-    sum_mod64 += func(start, stop);
+    uint64_t this_start = start+i*step_per_chunk;
+    uint64_t this_stop = this_start+step_per_chunk;
+    sum_mod64 += func(this_start, this_stop);
   }
   auto t2 = std::chrono::high_resolution_clock::now();
 
   std::cout << "Using " << fname
-    << ", sum of primes < " << step_per_chunk*nchunks
-    << " (mod 2^64) = " << sum_mod64
+    << ", sum of primes in ["
+    << start << ", " << start+step_per_chunk*nchunks
+    << "] (mod 2^64) = " << sum_mod64
     << " computed with " << nchunks
     << " chunks in " << (t2-t1)/1.0s << " s"
     << std::endl;
@@ -57,9 +59,13 @@ void TimeFunc(
 
 int main(int argc, char** argv)
 {
-  TimeFunc(func1, "func1", 1ull << 30, 1ull << 10);
-  TimeFunc(func1, "func1", 1ull << 40, 1ull << 0);
-  TimeFunc(func2, "func2", 1ull << 30, 1ull << 10);
+  TimeFunc(func1, "func1", 0, 1ull << 30, 1ull << 8);
+  TimeFunc(func1, "func1", 0, 1ull << 38, 1ull << 0);
+  TimeFunc(func2, "func2", 0, 1ull << 30, 1ull << 8);
+
+  TimeFunc(func1, "func1", 1ull << 47, 1ull << 30, 1ull << 8);
+  TimeFunc(func1, "func1", 1ull << 47, 1ull << 38, 1ull << 0);
+  TimeFunc(func2, "func2", 1ull << 47, 1ull << 30, 1ull << 8);
 
   return 0;
 }
